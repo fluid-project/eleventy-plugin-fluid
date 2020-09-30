cf456444e31147b5a8d7b2c8210b0cbb4fd8f594
Ned Zimmerman <ned@bight.dev>
2020-09-30 11:49:56 -0300

    chore: update all links to fluid-project/eleventy-plugin-fluid

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,12 @@
 Copyright the eleventy-plugin-fluid copyright holders.
 
 See the AUTHORS.md file at the top-level directory of this distribution and at
-https://github.com/greatislander/eleventy-plugin-fluid/raw/main/AUTHORS.md.
+https://github.com/fluid-project/eleventy-plugin-fluid/raw/main/AUTHORS.md.
 
 Licensed under the New BSD license. You may not use this file except in compliance with this License.
 
 You may obtain a copy of the New BSD License at
-https://github.com/greatislander/eleventy-plugin-fluid/raw/main/LICENSE.md.
+https://github.com/fluid-project/eleventy-plugin-fluid/raw/main/LICENSE.md.
 */
 "use strict";
 
