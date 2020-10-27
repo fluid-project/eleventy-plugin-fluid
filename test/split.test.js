1c84cdf6ff30335ab225c6f3140b61eacac6d147
Ned Zimmerman <ned@bight.dev>
2020-10-27 17:51:20 -0300

    feat: add split filter (#9)
    * feat: add split filter
    
    * docs: add doc

---
+++ test/split.test.js
@@ -0,0 +1,19 @@
+/*
+Copyright the eleventy-plugin-fluid copyright holders.
+
+See the AUTHORS.md file at the top-level directory of this distribution and at
+https://github.com/fluid-project/eleventy-plugin-fluid/raw/main/AUTHORS.md.
+
+Licensed under the New BSD license. You may not use this file except in compliance with this License.
+
+You may obtain a copy of the New BSD License at
+https://github.com/fluid-project/eleventy-plugin-fluid/raw/main/LICENSE.md.
+*/
+"use strict";
+
+const test = require("ava");
+const splitFilter = require("../src/filters/split-filter.js");
+
+test("Splits string into array", function (t) {
+    t.deepEqual(splitFilter("a,b,c", ","), ["a", "b", "c"]);
+});
