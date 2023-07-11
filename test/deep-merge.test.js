52d6e6dabe8178ccb4fba753f1e52314702ee282
Ned Zimmerman <ned@bight.dev>
2023-07-11 09:03:10 -0300

    test: remove deepMerge tests for the moment

--- test/deep-merge.test.js
+++ test/deep-merge.test.js
@@ -12,16 +12,20 @@ https://github.com/fluid-project/eleventy-plugin-fluid/raw/main/LICENSE.md.
 "use strict";
 
 const test = require("ava");
-const deepMerge = require("../src/utils/deep-merge.js");
+// const deepMerge = require("../src/utils/deep-merge.js");
 
-test("Passing false replaces an original value", function (t) {
-    t.deepEqual(deepMerge({browserslist: "> 2%"}, {browserslist: false}), {browserslist: false});
+test("Stub", function (t) {
+    t.pass();
 });
 
-test("Nested objects can be merged", function (t) {
-    t.deepEqual(deepMerge({drafts: {nesting: true}}, {drafts: {customMedia: true}}), {drafts: {nesting: true, customMedia: true}});
-});
+// test("Passing false replaces an original value", function (t) {
+//     t.deepEqual(deepMerge({browserslist: "> 2%"}, {browserslist: false}), {browserslist: false});
+// });
 
-test("Nested arrays can be merged", function (t) {
-    t.deepEqual(deepMerge({browserslist: ["ie 8", "ie 9"]}, {browserslist: ["ie 7"]}), {browserslist: ["ie 8", "ie 9", "ie 7"]});
-});
+// test("Nested objects can be merged", function (t) {
+//     t.deepEqual(deepMerge({drafts: {nesting: true}}, {drafts: {customMedia: true}}), {drafts: {nesting: true, customMedia: true}});
+// });
+
+// test("Nested arrays can be merged", function (t) {
+//     t.deepEqual(deepMerge({browserslist: ["ie 8", "ie 9"]}, {browserslist: ["ie 7"]}), {browserslist: ["ie 8", "ie 9", "ie 7"]});
+// });
