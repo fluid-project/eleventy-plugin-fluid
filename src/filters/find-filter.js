d93010e75f1d67677906c0411c04566287972356
Ned Zimmerman <ned@bight.dev>
2025-04-30 14:18:24 -0300

    feat: add find filter

---
+++ src/filters/find-filter.js
@@ -0,0 +1,17 @@
+/*
+Copyright the eleventy-plugin-fluid copyright holders.
+
+See the AUTHORS.md file at the top-level directory of this distribution and at
+https://github.com/fluid-project/eleventy-plugin-fluid/raw/main/AUTHORS.md.
+
+Licensed under the New BSD license. You may not use this file except in compliance with this License.
+
+You may obtain a copy of the New BSD License at
+https://github.com/fluid-project/eleventy-plugin-fluid/raw/main/LICENSE.md.
+*/
+
+import _ from "lodash";
+
+export default function findFilter(arr = [], key = "", value) {
+    return arr.find((post) => _.get(post, key) === value);
+};
