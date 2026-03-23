31227184fcc4b338caaf25ac90740d130651fd0c
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-03-23 09:35:59 -0300

    fix: resolve linting issues

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,9 @@ import eslintConfigInclusiveDesign from '@inclusive-design/eslint-config';
 export default defineConfig([
 	{
 		extends: [eslintConfigInclusiveDesign],
+		rules: {
+			camelcase: ['error', {properties: 'never'}],
+		},
 	},
 	{
 		ignores: ['coverage/**', '_site/*'],
