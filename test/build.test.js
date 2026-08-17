f7cfa0dcce3ed12b05d6c79d0533a1610acf2c17
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-08-17 09:33:48 -0300

    test: split assertions

--- test/build.test.js
+++ test/build.test.js
@@ -36,7 +36,8 @@ test('Builds minified JavaScript', async () => {
 
 test('Uses Markdown plugin', async () => {
 	const indexPage = fs.readFileSync('_site/index.html', 'utf8');
-	assert.ok(indexPage.includes('<dl><dt>Widdershins</dt><dd>Counter-clockwise.</dd></dl>'));
+	assert.ok(indexPage.includes('<dt>Widdershins</dt>'));
+	assert.ok(indexPage.includes('<dd>Counter-clockwise.</dd>'));
 });
 
 test('Uses Markdown plugin with options', async () => {
